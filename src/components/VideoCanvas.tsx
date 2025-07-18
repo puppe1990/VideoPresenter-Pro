f8643a05a0a8f083f8cea4f8126b4000cd32de94
puppe1990 <matheus.puppe90@hotmail.com>
2025-07-18 17:33:21 -0300

    resolve

--- src/components/VideoCanvas.tsx
+++ src/components/VideoCanvas.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import React from 'react'
 import { useEffect, useRef, useState, useCallback, forwardRef, useImperativeHandle } from 'react'
 import Image from 'next/image'
 import { PresenterSettings } from './VideoPresenter'
